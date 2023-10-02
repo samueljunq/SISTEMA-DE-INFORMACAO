b51e8bf14eaed11fd24ee54df63da9583025418f
Samuel Junqueira <101256901+samueljunq@users.noreply.github.com>
2023-10-02 08:24:38 -0300

    Add files via upload

---
+++ LTPIII/Apresentacao/src/CalculadoraDePrecos.java
@@ -0,0 +1,31 @@
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class CalculadoraDePrecos {
+    public static void main(String[] args) {
+        Map<String, Double> taxasImpostos = new HashMap<>();
+        taxasImpostos.put("ICMS", 0.07);
+        taxasImpostos.put("Confins", 0.12);
+        taxasImpostos.put("IPI", 0.08);
+        taxasImpostos.put("ISS", 0.05);
+        taxasImpostos.put("Cide", 0.10);
+        taxasImpostos.put("CSLL", 0.04);
+
+        List<Produto> produtos = new ArrayList<>();
+        produtos.add(new Produto("Arroz", 7.00, List.of("Confins", "ICMS", "ISS"), 0.20));
+        produtos.add(new Produto("Feijão", 7.00, List.of("Confins", "ICMS", "ISS"), 0.25));
+        produtos.add(new Produto("Carne", 7.00, List.of("Confins", "ICMS", "IPI"), 0.35));
+        produtos.add(new Produto("Cerveja", 7.00, List.of("Confins", "ICMS", "ISS", "IPI"), 0.30));
+        produtos.add(new Produto("Gás", 7.00, List.of("Confins", "ICMS", "IPI"), 0.15));
+        produtos.add(new Produto("Gasolina", 7.00, List.of("Confins", "ICMS", "Cide"), 0.15));
+
+        System.out.println("Lista de Preços de Venda:");
+        for (Produto produto : produtos) {
+            double precoVenda = produto.calcularPrecoVenda(taxasImpostos);
+            System.out.println(produto.getNome() + ": R$" + String.format("%.2f", precoVenda));
+        }
+    }
+}
---
+++ LTPIII/Apresentacao/src/Produto.java
@@ -0,0 +1,63 @@
+
+import java.util.List;
+import java.util.Map;
+
+public class Produto {
+	String nome;
+    double precoCusto;
+    List<String> impostos;
+    double margemLucro;
+
+    public Produto(String nome, double precoCusto, List<String> impostos, double margemLucro) {
+        this.nome = nome;
+        this.precoCusto = precoCusto;
+        this.impostos = impostos;
+        this.margemLucro = margemLucro;
+    }
+
+    public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public double getPrecoCusto() {
+		return precoCusto;
+	}
+
+	public void setPrecoCusto(double precoCusto) {
+		this.precoCusto = precoCusto;
+	}
+
+	public List<String> getImpostos() {
+		return impostos;
+	}
+
+	public void setImpostos(List<String> impostos) {
+		this.impostos = impostos;
+	}
+
+	public double getMargemLucro() {
+		return margemLucro;
+	}
+
+	public void setMargemLucro(double margemLucro) {
+		this.margemLucro = margemLucro;
+	}
+
+	public double calcularPrecoVenda(Map<String, Double> taxasImpostos) {
+	    double totalImpostos = 0;
+	    for (String imposto : impostos) {
+	        if (taxasImpostos.containsKey(imposto)) {
+	            totalImpostos += taxasImpostos.get(imposto);
+	        }
+	    }
+	    
+	    double precoVenda = precoCusto * (1 + totalImpostos); // Aplicar impostos sobre o preço de custo
+	    double precoComMargem = precoVenda * (1 + margemLucro); // Aplicar margem de lucro sobre o preço com impostos
+	    return precoComMargem;
+	}
+    
+}
